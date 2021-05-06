43506a87b69106e7d04e5e73bab87512e67b6385
Bwzerra <guilhermealvesbezerrax@gmail.com>
2021-05-06 20:08:45 -0300

    code improvements

--- organizer.js
+++ organizer.js
@@ -16,63 +16,63 @@ let apks = ["apk", "xapk", "exe", "deb"]
 let docx = ["docx", "doc", "pdf", "epub", "bok", "pptx", "xlsx", "txt"]
 let comps = ["zip", "rar", "tar", "gz", "xz", "iso", "7z", "bz2", "jar", "lzma"]
 let sc = ["html", "css", "js", "py", "cpp", "c", "xml", "json"]
-let exception = 'crdownload'
 
 process.chdir('/sdcard');
 
 // criando as pastas
-for (i = 0; i < folders.length; i++) {
-  if (!fs.existsSync(dl + folders[i])) {
-    fs.mkdirSync(dl + folders[i])
-    log(c.ok + "Folder successfully created:", c.head + dl + c.blue + folders[i])
+for (let fol in folders) {
+  if (!fs.existsSync(dl + folders[fol])) {
+    fs.mkdirSync(dl + folders[fol])
+    log(c.ok + "Folder successfully created:", c.head + dl + c.blue + folders[fol])
   }
 }
 
 // movendo os arquivos
-async function moveFiles() {
+function moveFiles() {
   fs.readdirSync('./').forEach(file => {
-    if (fs.statSync(file).isFile() == true) {
-      let hidden = file.startsWith('.');
+    if (fs.statSync(file).isFile()) {
       let ext = file.substr((file.lastIndexOf('.') + 1));
       let oldPath = dl + file;
-
-      try {
-        if (vids.includes(ext)) {
-          fs.rename(oldPath, dl + folders[0] + '/' + file, (err) => {
-if (err) throw err })
-
-          log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[0] + c.o)
-        } else if (imgs.includes(ext)) {
-          fs.rename(oldPath, dl + folders[1] + '/' + file, (err) => { if (err) throw err })
-
-          log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[1] + c.o)
-        } else if (auds.includes(ext)) {
-          fs.rename(oldPath, dl + folders[2] + '/' + file, (err) => { if (err) throw err })
-
-          log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[2] + c.o)
-        } else if (apks.includes(ext)) {
-          fs.rename(oldPath, dl + folders[3] + '/' + file, (err) => { if (err) throw err })
-
-          log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[3] + c.o)
-        } else if (docx.includes(ext)) {
-          fs.rename(oldPath, dl + folders[4] + '/' + file, (err) => { if (err) throw err })
-
-          log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[4] + c.o)
-        } else if (comps.includes(ext)) {
-          fs.rename(oldPath, dl + folders[5] + '/' + file, (err) => { if (err) throw err })
-
-          log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[5] + c.o)
-        } else if (sc.includes(ext)) {
-          fs.rename(oldPath, dl + folders[6] + '/' + file, (err) => { if (err) throw err })
-
-          log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[6] + c.o)
-        } else {
-          fs.rename(oldPath, dl + folders[7] + '/' + file, (err) => { if (err) throw err })
-
-          log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[7] + c.o)
-        }
-      } catch (e) {
-        log(`There is an error when trying to move the file: ${file}`)
+      
+      // movendo videos
+      if (vids.includes(ext)) {
+        fs.renameSync(oldPath, dl + folders[0] + '/' + file)
+        log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[0] + c.o)
+        
+        // movendo imagens
+      } else if (imgs.includes(ext)) {
+        fs.renameSync(oldPath, dl + folders[1] + '/' + file)
+        log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[1] + c.o)
+        
+        // movendo audios
+      } else if (auds.includes(ext)) {
+        fs.renameSync(oldPath, dl + folders[2] + '/' + file)
+        log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[2] + c.o)
+        
+        // movendo aplicativos
+      } else if (apks.includes(ext)) {
+        fs.renameSync(oldPath, dl + folders[3] + '/' + file)
+        log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[3] + c.o)
+        
+        // movendo documentos
+      } else if (docx.includes(ext)) {
+        fs.renameSync(oldPath, dl + folders[4] + '/' + file)
+        log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[4] + c.o)
+        
+        // movendo compactados
+      } else if (comps.includes(ext)) {
+        fs.renameSync(oldPath, dl + folders[5] + '/' + file)
+        log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[5] + c.o)
+        
+        // movendo scripts
+      } else if (sc.includes(ext)) {
+        fs.renameSync(oldPath, dl + folders[6] + '/' + file)
+        log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[6] + c.o)
+        
+        // movendo outros
+      } else {
+        fs.renameSync(oldPath, dl + folders[7] + '/' + file)
+        log(c.head + file, c.ok + "moved to 👉", c.blue + "Dowload >", folders[7] + c.o)
       }
     }
   });
